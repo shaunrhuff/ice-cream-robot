ec4e3fc8203ffb5fcf5fa69cef7c091d6b5b0137
shaunrhuff <101518963+shaunrhuff@users.noreply.github.com>
2022-10-01 22:56:09 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello welcome the ice cream store")
+print("")
+
+answer = input("What flavor would you like?: ")
+if answer == 'chocolate':
+    print("Chocolate is a great choice!")
+elif answer =='vanila':
+    print("Vanila is a great choice!")
+elif answer == 'strawberry':
+    print("Strawberry is a great choice!")
+else:
+    print("that is not a valid choice.")
